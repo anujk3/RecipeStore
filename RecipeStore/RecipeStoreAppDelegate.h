e91e7a6a4cb0fb46e83119ac533900a9a1a8a384
Anuj Katiyal <anujk3@gmail.com>
2014-09-16 21:41:41 +0530

    Initial Commit

---
+++ RecipeStore/RecipeStoreAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  RecipeStoreAppDelegate.h
+//  RecipeStore
+//
+//  Created by Anuj Katiyal on 16/09/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeStoreAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
